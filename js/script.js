fb0f0a6631d6e2fabfc321f9ccadf86e4318be0c
Alekseyev Eric <ericalex0207@gmail.com>
2024-11-01 19:35:02 +0200

    feat: move init slider in file sliders.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
 'use strict'
 import { initMenu } from './menu.js'
 import { initFixedHeader } from './fixed-header.js'
-import { SliderCard } from './SliderCard.js'
+import { initSliders } from './sliders.js'
 
 window.addEventListener('load', windowLoaded)
 
 function windowLoaded() {
 	initMenu(400)
 	initFixedHeader()
-	const sliderMedia = new Map()
-	sliderMedia.set(1150, {
-		gap: 40,
-		amountSlide: 2
-	})
-	sliderMedia.set(620, {
-		amountSlide: 1,
-		speedAnimation: 400
-	})
-
-	new SliderCard(
-		'.pets__slider',
-		3,
-		90,
-		600,
-		sliderMedia
-	)
+	initSliders()
 }
