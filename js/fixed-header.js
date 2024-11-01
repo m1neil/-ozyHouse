8e5e16d54cd988f3e3c4ea9fa74968327a3b8332
Alekseyev Eric <ericalex0207@gmail.com>
2024-11-01 19:28:58 +0200

    feat: add class SliderCard, add section pets

--- js/fixed-header.js
+++ js/fixed-header.js
@@ -2,12 +2,13 @@ export function initFixedHeader() {
 	const header = document.querySelector('header.header')
 	const heightTrigger = header.getAttribute('data-header-fixed') ?
 		parseFloat(header.getAttribute('data-header-fixed')) : 100
-	window.addEventListener('scroll', () => {
-		console.log(scrollY)
+	window.addEventListener('scroll', changeFixed)
+	changeFixed()
 
+	function changeFixed() {
 		if (scrollY > heightTrigger && !header.classList.contains('--fixed'))
 			header.classList.add('--fixed')
 		else if (scrollY < heightTrigger && header.classList.contains('--fixed'))
 			header.classList.remove('--fixed')
-	})
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 'use strict'
 import { initMenu } from './menu.js'
 import { initFixedHeader } from './fixed-header.js'
+import { SliderCard } from './SliderCard.js'
 
 window.addEventListener('load', windowLoaded)
 
 function windowLoaded() {
 	initMenu(400)
 	initFixedHeader()
+	const sliderMedia = new Map()
+	sliderMedia.set(1150, {
+		gap: 40,
+		amountSlide: 2
+	})
+	sliderMedia.set(620, {
+		amountSlide: 1,
+		speedAnimation: 400
+	})
+
+	new SliderCard(
+		'.pets__slider',
+		3,
+		90,
+		600,
+		sliderMedia
+	)
 }
